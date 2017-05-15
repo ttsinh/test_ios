af63da42d666b062778dabb9a64c95ce19ddd2fb
cuscedu <cuscedu@mac06.local>
2017-05-15 16:00:55 +0700

    Initial Commit

---
+++ baitap3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  baitap3
+//
+//  Created by cuscedu on 5/15/17.
+//  Copyright © 2017 cuscedu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ baitap3Tests/baitap3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  baitap3Tests.swift
+//  baitap3Tests
+//
+//  Created by cuscedu on 5/15/17.
+//  Copyright © 2017 cuscedu. All rights reserved.
+//
+
+import XCTest
+@testable import baitap3
+
+class baitap3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
